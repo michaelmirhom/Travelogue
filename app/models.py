9888b4fe9733ca5e7fb77ed7fe6b2b76a3c37d9d
michael mirhom <micharby73@gmail.com>
2023-09-24 20:26:02 -0500

    Adding Realtion

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
 from app import db
 from datetime import datetime
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))  
+    password_hash = db.Column(db.String(128)) 
+    trips = db.relationship('Trip', backref='traveler', lazy=True)
+    reviews = db.relationship('Review', backref='reviewer', lazy=True)
 
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -13,13 +16,21 @@ class Trip(db.Model):
     end_date = db.Column(db.DateTime)
     photos = db.Column(db.String(500))  
     description = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    reviews = db.relationship('Review', backref='trip', lazy=True)
+
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text, nullable=False)
-    rating = db.Column(db.Integer, nullable=False)    
+    rating = db.Column(db.Integer, nullable=False)  
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    trip_id = db.Column(db.Integer, db.ForeignKey('trip.id'))
+
 class Destination(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     country = db.Column(db.String(100), nullable=False)
     attractions = db.Column(db.Text)
+    reviews = db.relationship('Review', backref='destination', lazy=True)
+
 
