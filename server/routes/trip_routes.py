f4e9c4a41cee0e612edb9e295e886f3269c0a3db
michael mirhom <micharby73@gmail.com>
2023-09-26 18:33:44 -0500

    Create trip method

--- server/routes/trip_routes.py
+++ server/routes/trip_routes.py
@@ -3,3 +3,18 @@ from config import db
 from models import Trip
 
 trip_routes = Blueprint('trip_routes', __name__)
+
+@trip_routes.route('/trips', methods=['POST'])
+def create_trip():
+    data = request.get_json()
+    new_trip = Trip(
+        destination=data['destination'],
+        start_date=data.get('start_date'),
+        end_date=data.get('end_date'),
+        photos=data.get('photos'),
+        description=data.get('description'),
+        user_id=data['user_id']
+    )
+    db.session.add(new_trip)
+    db.session.commit()
+    return jsonify(new_trip.to_dict()), 201
