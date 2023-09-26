bcbc3c5c1361ca794681971f104241dac486c4b3
michael mirhom <micharby73@gmail.com>
2023-09-26 16:18:29 -0500

    Update my user_routes.py

--- server/routes/user_routes.py
+++ server/routes/user_routes.py
@@ -1,10 +1,9 @@
-# app/routes/user_routes.py
-
 from flask import Blueprint, jsonify, request
-from app import db
-from app.models import User
+from server.config import db
+from server.models import User
 
 user_routes = Blueprint('user_routes', __name__)
+
 @user_routes.route('/users', methods=['POST'])
 def create_user():
     data = request.get_json()
@@ -16,16 +15,19 @@ def create_user():
     db.session.add(new_user)
     db.session.commit()
     return jsonify(new_user.to_dict()), 201
+
 @user_routes.route('/users', methods=['GET'])
 def get_users():
     users = User.query.all()
     return jsonify([user.to_dict() for user in users])
+
 @user_routes.route('/users/<int:id>', methods=['GET'])
 def get_user(id):
     user = User.query.get(id)
     if not user:
         return jsonify({"error": "User not found"}), 404
     return jsonify(user.to_dict())
+
 @user_routes.route('/users/<int:id>', methods=['PUT'])
 def update_user(id):
     user = User.query.get(id)
@@ -48,11 +50,12 @@ def update_user(id):
         user.email = data['email']
 
     if 'password' in data and data['password']:
-        user.set_password(data['password']) 
+        user.set_password(data['password'])  
 
     db.session.commit()
 
     return jsonify(user.to_dict()), 200
+
   
 
     
