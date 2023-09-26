6f91273c135e30ea474f252aba14efab8452a5d0
michael mirhom <micharby73@gmail.com>
2023-09-26 18:38:03 -0500

    Updates a specific trip by its ID

--- server/routes/trip_routes.py
+++ server/routes/trip_routes.py
@@ -28,3 +28,18 @@ def get_trip(id):
     if not trip:
         return jsonify({"error": "Trip not found"}), 404
     return jsonify(trip.to_dict())
+@trip_routes.route('/trips/<int:id>', methods=['PUT'])
+def update_trip(id):
+    trip = Trip.query.get(id)
+    if not trip:
+        return jsonify({"error": "Trip not found"}), 404
+    data = request.json
+
+    trip.destination = data.get('destination', trip.destination)
+    trip.start_date = data.get('start_date', trip.start_date)
+    trip.end_date = data.get('end_date', trip.end_date)
+    trip.photos = data.get('photos', trip.photos)
+    trip.description = data.get('description', trip.description)
+
+    db.session.commit()
+    return jsonify(trip.to_dict()), 200
