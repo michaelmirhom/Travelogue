b78c845b71e8bcebe164e2d60b1140a1ef2571a4
michael mirhom <micharby73@gmail.com>
2023-09-26 16:14:44 -0500

    Update my app.py

--- server/app.py
+++ server/app.py
@@ -1,26 +1,26 @@
 
-from server.config import app, db
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from server.config import db
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/travelogue.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
 from server.models import User, Trip, Review, Destination
 from server.routes.user_routes import user_routes
 
 app.register_blueprint(user_routes, url_prefix='/api')
 
+
 if __name__ == "__main__":
     app.run(debug=True, port=5555)
 
-
-
-app = Flask(__name__)
-
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///travelogue.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-
-from app.models import User, Trip, Review, Destination
-from app.routes import user_routes
-app.register_blueprint(user_routes.user_routes, url_prefix='/api')
 
