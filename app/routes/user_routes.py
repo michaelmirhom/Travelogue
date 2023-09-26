d071495e0a26b1b46f276cd9b13b2cf44a4c057b
michael mirhom <micharby73@gmail.com>
2023-09-26 14:49:14 -0500

    Create a new user (Register)

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -5,3 +5,15 @@ from app import db
 from app.models import User
 
 user_routes = Blueprint('user_routes', __name__)
+@user_routes.route('/users', methods=['POST'])
+def create_user():
+    data = request.get_json()
+    new_user = User(
+        username=data['username'],
+        email=data['email'],
+        password_hash=data['password_hash']  
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify(new_user.to_dict()), 201
+@user_routes.route('/users', methods=['GET'])
