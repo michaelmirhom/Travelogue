350b3fb98edfc17c4cb8e31810d9a91fcff2d632
michael mirhom <micharby73@gmail.com>
2023-09-26 14:24:49 -0500

    Create route file

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+
+
 app = Flask(__name__)
 
 
@@ -12,3 +14,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
+from app.models import User, Trip, Review, Destination
