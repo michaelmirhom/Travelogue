8d9d56267cd8b180112f41a2b679068ea42dc14d
michael mirhom <micharby73@gmail.com>
2023-09-26 16:08:28 -0500

    Update config.py

--- server/app.py
+++ server/app.py
@@ -1,8 +1,12 @@
-# app/__init__.py
 
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from server.config import app, db
+from server.models import User, Trip, Review, Destination
+from server.routes.user_routes import user_routes
+
+app.register_blueprint(user_routes, url_prefix='/api')
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5555)
 
 
 
