7c1bece93a638d39e8f2ffa3f92eced74281002f
michael mirhom <micharby73@gmail.com>
2023-09-27 18:54:11 -0500

    Make a POST request to the login endpoint with user credentials

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,3 +1,32 @@
 const handleLogin = (e) => {
-    
+
     e.preventDefault();
+
+    
+    fetch('http://localhost:5555/api/users/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+    })
+    .then((response) => {
+     
+        return response.json();
+    })
+    .then((data) => {
+       
+        if (data.token) {
+           
+            localStorage.setItem('authToken', data.token);
+           
+        } else {
+          
+            console.error("Login failed:", data.message);
+        }
+    })
+    .catch((error) => {
+    
+        console.error("There was an error logging in:", error);
+    });
+};
