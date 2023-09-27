a149d2b1283fdc98a79c63f73a74b38739a62346
michael mirhom <micharby73@gmail.com>
2023-09-26 19:11:40 -0500

    Creates a new review

--- server/routes/review_routes.py
+++ server/routes/review_routes.py
@@ -3,3 +3,16 @@ from config import db
 from models import Review
 
 review_routes = Blueprint('review_routes', __name__)
+@review_routes.route('/reviews', methods=['POST'])
+def create_review():
+    data = request.get_json()
+    new_review = Review(
+        content=data['content'],
+        rating=data['rating'],
+        user_id=data['user_id'],
+        trip_id=data.get('trip_id'),
+        destination_id=data.get('destination_id')
+    )
+    db.session.add(new_review)
+    db.session.commit()
+    return jsonify(new_review.to_dict()), 201
