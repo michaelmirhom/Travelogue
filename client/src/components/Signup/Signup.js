1a925d271e98ee421bc2805f22a64c217aba2232
michael mirhom <micharby73@gmail.com>
2023-09-27 19:18:13 -0500

    Send the signup details to the server via a POST request

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -16,3 +16,10 @@ const Signup = () => {
         return;
 
         }
+        fetch('http://localhost:5555/api/users/signup', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, email, password }),
+        })
