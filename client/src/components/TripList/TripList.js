26d328ba67629bd275fb2963054add372f9021e3
michael mirhom <micharby73@gmail.com>
2023-09-28 17:48:02 -0500

    useEffect hook to fetch trips once the component mounts

--- client/src/components/TripList/TripList.js
+++ client/src/components/TripList/TripList.js
@@ -1,7 +1,17 @@
 import React, { useState, useEffect } from 'react';
 const TripList = () => {
-    // State variables for trips, loading state, and potential errors
+    
     const [trips, setTrips] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
-}
+    useEffect(() => {
+        
+        fetch('http://localhost:5555/api/trips') 
+            .then(response => {
+                
+                if (!response.ok) {
+                    throw new Error('Failed to fetch trips');
+                }
+                
+                return response.json();
+            })
