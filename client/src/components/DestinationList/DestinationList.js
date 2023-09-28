99f39d1985af8010912110611cb742f9da093390
michael mirhom <micharby73@gmail.com>
2023-09-28 18:24:33 -0500

    Export the DestinationList component

--- client/src/components/DestinationList/DestinationList.js
+++ client/src/components/DestinationList/DestinationList.js
@@ -1,58 +1,53 @@
 import React, { useState, useEffect } from 'react';
+
 const DestinationList = () => {
-    
     const [destinations, setDestinations] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
-    useEffect(() => {
-        
-        fetch('http://localhost:5555/api/destinations') 
 
+    useEffect(() => {
+        // Fetch destinations from your API
+        fetch('http://localhost:5555/api/destinations') 
             .then(response => {
-               
                 if (!response.ok) {
                     throw new Error('Failed to fetch destinations');
                 }
-               
-                return response.json()
+                return response.json();
             })
             .then(data => {
-                
                 setDestinations(data);
-                
                 setIsLoading(false);
             })
             .catch(err => {
-              
                 setError(err.message);
                 setIsLoading(false);
             });
-    }, []); 
-        
+    }, []);
+
     if (isLoading) {
         return <div>Loading...</div>;
     }
 
-
     if (error) {
         return <div>Error: {error}</div>;
-        
-        return (
-            <div>
-                <h2>Destinations</h2>
-               
-                {destinations.map(destination => (
-                    <div key={destination.id}>
-                        <h3>{destination.name}</h3>
-                        <p>Country: {destination.country}</p>
-                       
-                        <p>Attractions: {destination.attractions}</p>
-                    </div>
-                ))}
-            </div>
-        );
     }
-    
+
+    return (
+        <div>
+            <h2>Destinations</h2>
+            {destinations.map(destination => (
+                <div key={destination.id}>
+                    <h3>{destination.name}</h3>
+                    <p>Country: {destination.country}</p>
+                    <p>Attractions: {destination.attractions}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default DestinationList;
+
     
             
          
