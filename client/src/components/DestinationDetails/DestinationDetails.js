48fbd18b418a1c364dbb49f8653d7cad0016d525
michael mirhom <micharby73@gmail.com>
2023-09-28 18:50:18 -0500

    Dependency array with 'id' ensures the useEffect hook reruns if the ID changes

--- client/src/components/DestinationDetails/DestinationDetails.js
+++ client/src/components/DestinationDetails/DestinationDetails.js
@@ -30,6 +30,8 @@ const DestinationDetails = () => {
                       
                        setIsLoading(false);
                     });
+                }, [id]); 
+
                      
                
              
