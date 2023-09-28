ff9703d9df70092247cba09ccef12302e1856b2f
michael mirhom <micharby73@gmail.com>
2023-09-27 19:01:33 -0500

    Update my login.js code to include  validation and Error handling

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,32 +1,70 @@
-const handleLogin = (e) => {
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
-    e.preventDefault();
-
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
     
-    fetch('http://localhost:5555/api/users/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-    })
-    .then((response) => {
-     
-        return response.json();
-    })
-    .then((data) => {
-       
-        if (data.token) {
-           
-            localStorage.setItem('authToken', data.token);
-           
-        } else {
-          
-            console.error("Login failed:", data.message);
+    const history = useHistory();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        // Form validation
+        if (!email || !password) {
+            setErrorMessage('Please provide both email and password.');
+            return;
         }
-    })
-    .catch((error) => {
-    
-        console.error("There was an error logging in:", error);
-    });
-};
+
+        fetch('http://localhost:5555/api/users/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ email, password }),
+        })
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            if (data.token) {
+                localStorage.setItem('authToken', data.token);
+                history.push('/dashboard'); // redirecting to dashboard
+            } else {
+                setErrorMessage(data.message || "Login failed.");
+            }
+        })
+        .catch((error) => {
+            setErrorMessage("There was an error logging in: " + error.message);
+        });
+    };
+
+    return (
+        <div>
+            <h2>Login Page</h2>
+            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+            <form onSubmit={handleLogin}>
+                <input
+                    type="email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
+
