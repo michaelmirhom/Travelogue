c4d9899feab696d60a644e14aaa12b0d45a0fcd5
michael mirhom <micharby73@gmail.com>
2023-09-28 17:45:52 -0500

    State variables for trips, loading state, and potential errors

--- client/src/components/TripList/TripList.js
+++ client/src/components/TripList/TripList.js
@@ -1,1 +1,7 @@
 import React, { useState, useEffect } from 'react';
+const TripList = () => {
+    // State variables for trips, loading state, and potential errors
+    const [trips, setTrips] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+}
