4a98133383278679151a0e418b6d2c2e957226fc
michael mirhom <micharby73@gmail.com>
2023-09-28 18:53:40 -0500

    Export the DestinationDetails component for use in other parts of the application

--- client/src/components/DestinationDetails/DestinationDetails.js
+++ client/src/components/DestinationDetails/DestinationDetails.js
@@ -1,59 +1,57 @@
-
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+
 const DestinationDetails = () => {
-    
     const [destination, setDestination] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
+
+   
     const { id } = useParams();
+
     useEffect(() => {
         
         fetch(`http://localhost:5555/api/destinations/${id}`)
             .then(response => {
-               
                 if (!response.ok) {
                     throw new Error('Failed to fetch destination details');
                 }
-               
                 return response.json();
             })
             .then(data => {
-              
                 setDestination(data);
-               
                 setIsLoading(false);
             })
             .catch(err => {
-                   
-                   setError(err.message);
-                      
-                       setIsLoading(false);
-                    });
-                }, [id]); 
-              
+                setError(err.message);
+                setIsLoading(false);
+            });
+    }, [id]);
+
     if (isLoading) {
         return <div>Loading...</div>;
     }
 
-         
     if (error) {
         return <div>Error: {error}</div>;
-    }         
-    
-     if (!destination) {
+    }
+
+    if (!destination) {
         return <div>Destination not found.</div>;
     }
 
- return (
-    <div>
-        <h2>{destination.name}</h2>
-        <p>Country: {destination.country}</p>
-   
-   
-    </div>
-);
+    return (
+        <div>
+            <h2>{destination.name}</h2>
+            <p>Country: {destination.country}</p>
+            <p>Attractions: {destination.attractions}</p>
+           
+        </div>
+    );
 }
+
+export default DestinationDetails;
+
                
              
             
