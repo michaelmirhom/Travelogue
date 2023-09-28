8b40b908898c516cb11ae278c8395ad8e787aad5
michael mirhom <micharby73@gmail.com>
2023-09-27 20:08:13 -0500

    useEffect hook to run side effects, such as API calls, after the component mounts

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -3,3 +3,9 @@ const Dashboard = () => {
     
     const [trips, setTrips] = useState([]);
     const [destinations, setDestinations] = useState([]);
+    useEffect(() => {
+        
+        fetch('http://localhost:5555/api/trips') 
+            .then(response => response.json()) 
+            .then(data => setTrips(data)) 
+            .catch(err => console.error('Error fetching trips:', err)); 
