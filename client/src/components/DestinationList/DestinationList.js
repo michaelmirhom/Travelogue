9c8a0c69300253b04a8f1c61552653ff3e91448d
michael mirhom <micharby73@gmail.com>
2023-09-28 18:16:39 -0500

    Set State variables for destinations, loading state, and potential errors

--- client/src/components/DestinationList/DestinationList.js
+++ client/src/components/DestinationList/DestinationList.js
@@ -1,1 +1,6 @@
 import React, { useState, useEffect } from 'react';
+const DestinationList = () => {
+    
+    const [destinations, setDestinations] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
