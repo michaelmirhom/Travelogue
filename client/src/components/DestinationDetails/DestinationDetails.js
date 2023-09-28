656dda69cdbb28c910a9014d675130d98602fbf0
michael mirhom <micharby73@gmail.com>
2023-09-28 18:44:47 -0500

    Initialize state variables for destination, loading state, and potential errors

--- client/src/components/DestinationDetails/DestinationDetails.js
+++ client/src/components/DestinationDetails/DestinationDetails.js
@@ -1,3 +1,8 @@
 
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+const DestinationDetails = () => {
+    
+    const [destination, setDestination] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
