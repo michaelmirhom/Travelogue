5b71097a996f6d1d693afdcc995107a0f8b82756
michael mirhom <micharby73@gmail.com>
2023-09-27 19:19:15 -0500

    If the response isn't a 2xx success code, throw an error

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -23,3 +23,10 @@ const Signup = () => {
             },
             body: JSON.stringify({ username, email, password }),
         })
+        .then((response) => {
+            
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); 
+        })
