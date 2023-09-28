c5ab7e330e2020f0fa63815bd190388163a3dabd
michael mirhom <micharby73@gmail.com>
2023-09-27 19:10:11 -0500

    State hooks for username, email, password, and any potential error messages

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,2 +1,9 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+const Signup = () => {
+    
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+    
