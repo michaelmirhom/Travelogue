b7dbd85e122b4187558fe3411ac4b0b0c32883fb
michael mirhom <micharby73@gmail.com>
2023-09-28 18:48:49 -0500

    Update the loading state to false since data is fetched

--- client/src/components/DestinationDetails/DestinationDetails.js
+++ client/src/components/DestinationDetails/DestinationDetails.js
@@ -21,14 +21,9 @@ const DestinationDetails = () => {
             .then(data => {
               
                 setDestination(data);
-                // Update the loading state to false since data is fetched
+               
                 setIsLoading(false);
             })
             .catch(err => {
-                // Update the error state with the error message
-                setError(err.message);
-                // Update the loading state to false since there was an error fetching data
-                setIsLoading(false);
-            });
              
             
