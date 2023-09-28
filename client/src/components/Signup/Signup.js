f53dc23723c3f32d7d8fe24076731c76abca5aa7
michael mirhom <micharby73@gmail.com>
2023-09-27 19:25:12 -0500

    Export the Signup component for use in other parts of the application

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,21 +1,23 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
+
 const Signup = () => {
-    
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    
     const history = useHistory();
-    const handleSignup = (e) => {
-       
-        e.preventDefault();
-       
-       if (!username || !email || !password) {
-        setErrorMessage('Please provide a username, email, and password.');
-        return;
 
+    const handleSignup = (e) => {
+        e.preventDefault();
+
+        // Form validation
+        if (!username || !email || !password) {
+            setErrorMessage('Please provide a username, email, and password.');
+            return;
         }
+
         fetch('http://localhost:5555/api/users/signup', {
             method: 'POST',
             headers: {
@@ -24,33 +26,28 @@ const Signup = () => {
             body: JSON.stringify({ username, email, password }),
         })
         .then((response) => {
-            
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
-            return response.json(); 
+            return response.json();
         })
         .then((data) => {
-            
             if (data.success) {
-                history.push('/login');
+                history.push('/login'); // redirecting to login
             } else {
-               
                 setErrorMessage(data.message || "Signup failed.");
             }
         })
         .catch((error) => {
-        
             setErrorMessage("There was an error during signup: " + error.message);
         });
     };
+
     return (
         <div>
             <h2>Signup Page</h2>
-          
             {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
             <form onSubmit={handleSignup}>
-               
                 <input
                     type="text"
                     placeholder="Username"
@@ -69,9 +66,10 @@ const Signup = () => {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
-                
                 <button type="submit">Signup</button>
             </form>
         </div>
     );
 }
+
+export default Signup;
