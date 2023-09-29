ea669f83983d8f44ebebdc411b78eac153f54fff
michael mirhom <micharby73@gmail.com>
2023-09-28 19:26:49 -0500

    Navigation bar with links to the main sections of the app

--- client/src/App.js
+++ client/src/App.js
@@ -7,3 +7,15 @@ import TripList from './components/TripList/TripList';
 import TripDetails from './components/TripDetails/TripDetails';
 import DestinationList from './components/DestinationList/DestinationList';
 import DestinationDetails from './components/DestinationDetails/DestinationDetails';
+function App() {
+  return (
+    <Router>
+      <div>
+       
+        <nav style={{ padding: '1rem', borderBottom: '1px solid gray' }}>
+          <Link to="/dashboard" style={{ margin: '0 1rem' }}>Dashboard</Link>
+          <Link to="/login" style={{ margin: '0 1rem' }}>Login</Link>
+          <Link to="/signup" style={{ margin: '0 1rem' }}>Signup</Link>
+          <Link to="/trips" style={{ margin: '0 1rem' }}>Trips</Link>
+          <Link to="/destinations" style={{ margin: '0 1rem' }}>Destinations</Link>
+        </nav>
