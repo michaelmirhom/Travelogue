11f7de7e966450d54d7efa1875bb666918399124
michael mirhom <micharby73@gmail.com>
2023-09-28 21:15:28 -0500

    flad db upgrade

--- server/config.py
+++ server/config.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"])
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////home/mirhom/phase5/Travelogue/server/instance/travelogue.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
