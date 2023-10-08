0c15e725d7a5ca42f341a3ba9a12c7243712fa74
michael mirhom <micharby73@gmail.com>
2023-10-08 00:46:18 -0500

    Added start_date, end_date, and description for seeded trips

--- server/seed.py
+++ server/seed.py
@@ -1,29 +1,26 @@
 from config import app, db
 from models import User, Trip, Destination, Review
 from werkzeug.security import generate_password_hash
+from datetime import datetime, timedelta
 
 def seed_data():
     
     db.drop_all()
     db.create_all()
 
-    
     user1 = User(username='johnDoe', email='john@example.com', password_hash=generate_password_hash('password123'))
     user2 = User(username='janeDoe', email='jane@example.com', password_hash=generate_password_hash('password456'))
 
-    
     dest1 = Destination(name='Giza', country='Egypt', attractions='Pyramids, River Nile', image_url='https://i.pinimg.com/736x/91/1d/16/911d167e6ceef11ccaad893686b42b18--visit-egypt-ancient-architecture.jpg')
-    dest2 = Destination(name='New York', country=' United States', attractions='Statue of Liberty',image_url='https://www.americamagazine.org/sites/default/files/main_image/iStock-476825760.jpg')
+    dest2 = Destination(name='New York', country='United States', attractions='Statue of Liberty', image_url='https://www.americamagazine.org/sites/default/files/main_image/iStock-476825760.jpg')
 
-    
-    trip1 = Trip(destination='Giza', user_id=1)
-    trip2 = Trip(destination='New York', user_id=2)
+ 
+    trip1 = Trip(destination='Giza', user_id=1, start_date=datetime.now(), end_date=datetime.now() + timedelta(days=7), description="A week exploring the wonders of Egypt!")
+    trip2 = Trip(destination='New York', user_id=2, start_date=datetime.now(), end_date=datetime.now() + timedelta(days=5), description="A quick trip to the Big Apple!")
 
-    
     review1 = Review(content='Loved Giza!', rating=5, user_id=1, trip_id=1, destination_id=1)
     review2 = Review(content='New York was bustling and vibrant!', rating=4, user_id=2, trip_id=2, destination_id=2)
 
-    
     db.session.add(user1)
     db.session.add(user2)
     db.session.add(dest1)
@@ -33,9 +30,9 @@ def seed_data():
     db.session.add(review1)
     db.session.add(review2)
 
-
     db.session.commit()
 
 if __name__ == '__main__':
     with app.app_context():
         seed_data()
+
