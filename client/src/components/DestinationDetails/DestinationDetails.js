516c17999c76312fb1283680049a923547e13999
michael mirhom <micharby73@gmail.com>
2023-10-08 01:06:31 -0500

    Render destination details and review addition form

--- client/src/components/DestinationDetails/DestinationDetails.js
+++ client/src/components/DestinationDetails/DestinationDetails.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { useParams, Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 
 const DestinationDetails = () => {
+    
     const [destination, setDestination] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
     const { id } = useParams();
 
+   
     const [newReviewContent, setNewReviewContent] = useState('');
     const [newReviewRating, setNewReviewRating] = useState(5);
 
+   
     const handleAddReview = () => {
         fetch('http://localhost:5555/api/reviews', {
             method: 'POST',
@@ -17,17 +20,18 @@ const DestinationDetails = () => {
             body: JSON.stringify({
                 content: newReviewContent,
                 rating: newReviewRating,
-                user_id: 1,  // TODO: Fetch the logged in user's ID
+                user_id: 1,  
                 destination_id: destination.id
             })
         })
         .then(response => response.json())
         .then(data => {
-            // TODO: Update reviews list in state if required
+            
             setNewReviewContent('');
             setNewReviewRating(5);
         });
     };
+
 
     useEffect(() => {
         fetch(`http://localhost:5555/api/destinations/${id}`)
@@ -59,6 +63,7 @@ const DestinationDetails = () => {
         return <div>Destination not found.</div>;
     }
 
+    
     return (
         <div className="destination-details" style={{ backgroundImage: `url(${destination.image_url})` }}>
             <h2>{destination.name}</h2>
@@ -87,52 +92,6 @@ const DestinationDetails = () => {
 }
 
 export default DestinationDetails;
-
-
-export const DestinationList = () => {
-    const [destinations, setDestinations] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
-
-    useEffect(() => {
-        fetch('http://localhost:5555/api/destinations')
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Failed to fetch destinations');
-                }
-                return response.json();
-            })
-            .then(data => {
-                setDestinations(data);
-                setIsLoading(false);
-            })
-            .catch(err => {
-                setError(err.message);
-                setIsLoading(false);
-            });
-    }, []);
-
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
-
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
-
-    return (
-        <div>
-            <h2>Destinations</h2>
-            {destinations.map(destination => (
-                <div key={destination.id}>
-                    <h3><Link to={`/destination/${destination.id}`}>{destination.name}</Link></h3>
-                    <p>Country: {destination.country}</p>
-                    <p>Attractions: {destination.attractions}</p>
-                </div>
-            ))}
-        </div>
-    );
-};
 
 
                
