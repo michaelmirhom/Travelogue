0cfc0c1b77a97ae81c27245f65838df2e4798f24
michael mirhom <micharby73@gmail.com>
2023-10-07 23:41:24 -0500

    Update my image URl

--- server/seed.py
+++ server/seed.py
@@ -12,16 +12,16 @@ def seed_data():
     user2 = User(username='janeDoe', email='jane@example.com', password_hash=generate_password_hash('password456'))
 
     
-    dest1 = Destination(name='Giza', country='Egypt', attractions='Pyramids, River Nile', image_url='https://i.pinimg.com/originals/9d/bb/e8/9dbbe803857060a7f3d17350302c75a4.jpg')
-    dest2 = Destination(name='Tokyo', country='Japan', attractions='Tokyo Tower, Senso-ji Temple',image_url='https://d1bv4heaa2n05k.cloudfront.net/user-images/1471601551497/shutterstock-194861771_main_1471601557463.jpeg')
+    dest1 = Destination(name='Giza', country='Egypt', attractions='Pyramids, River Nile', image_url='https://i.pinimg.com/736x/91/1d/16/911d167e6ceef11ccaad893686b42b18--visit-egypt-ancient-architecture.jpg')
+    dest2 = Destination(name='New York', country=' United States', attractions='Statue of Liberty',image_url='https://www.americamagazine.org/sites/default/files/main_image/iStock-476825760.jpg')
 
     
     trip1 = Trip(destination='Giza', user_id=1)
-    trip2 = Trip(destination='Tokyo', user_id=2)
+    trip2 = Trip(destination='New York', user_id=2)
 
     
     review1 = Review(content='Loved Giza!', rating=5, user_id=1, trip_id=1, destination_id=1)
-    review2 = Review(content='Tokyo was bustling and vibrant!', rating=4, user_id=2, trip_id=2, destination_id=2)
+    review2 = Review(content='New York was bustling and vibrant!', rating=4, user_id=2, trip_id=2, destination_id=2)
 
     
     db.session.add(user1)
